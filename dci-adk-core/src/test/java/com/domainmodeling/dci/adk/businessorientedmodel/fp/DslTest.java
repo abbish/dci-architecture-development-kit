aa71d9506e5d8ba6f558161ddcb939a999ee33b4
abbish <me@abbish.com>
2020-07-14 20:02:01 +0800

    remove dci-adk-spring-boot-starter

---
+++ dci-adk-core/src/test/java/com/domainmodeling/dci/adk/businessorientedmodel/fp/DslTest.java
@@ -0,0 +1,16 @@
+package com.domainmodeling.dci.adk.businessorientedmodel.fp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DslTest {
+
+    @Test
+    public void shoudCreateRequestForProposal() {
+
+//        __(Dsl::requestProposal, "abbish");
+
+        assertEquals(1, 1);
+    }
+}
